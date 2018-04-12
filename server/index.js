c19624ff029de136d5cb498f39225aa044f1f218
Steve Chang <stevenjchang@gmail.com>
2018-04-11 19:10:55 -0700

    setup client - react

--- server/index.js
+++ server/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = require('express').Router();
 const IP = 'localhost';
 const PORT = '3000';
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello from z-server!');
@@ -13,7 +19,7 @@ app.listen(PORT, (err) => {
   if (err) {
     console.log('server failed');
   }
-  console.log('server is listening on ${PORT}');
+  console.log(`server is listening on ${PORT}`);
 })
 
 module.exports = app;
