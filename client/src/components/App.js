6a433158cf2c96e220351e95d3c03a2544e39f77
Steve Chang <stevenjchang@gmail.com>
2018-04-11 20:03:07 -0700

    implement http request to google

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import axios from 'axios';
+
 import BookList from './BookList';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -7,6 +10,18 @@ class App extends React.Component {
     this.state = {
       books: [{id: 1}, {id: 2}, {id: 3}],
     };
+  }
+
+  componentDidMount() {
+    this.getBooksFromGoogle();
+  }
+
+  getBooksFromGoogle() {
+    axios.get('https://www.googleapis.com/books/v1/volumes?q=software&maxResults=40')
+      .then((results) => {
+        let data = results.data.items;
+        this.setState({ books: data});
+      })
   }
   render() {
     return (
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -4,7 +4,7 @@ import BookListItem from './BookListItem';
 const BookList = ({books}) => (
   <div>
     <h2>BookList</h2>
-    { books.map(item => <BookListItem book={item} />) }
+    { books.map(item => <BookListItem book={item} key={item.etag} />) }
   </div>
 )
 
