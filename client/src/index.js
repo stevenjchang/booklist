36ad00ffbc6dc39f8cce378d12a275ff61168b5b
Steve Chang <stevenjchang@gmail.com>
2018-04-11 23:12:13 -0700

    finish setup redux (after successful npm install)

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import App from './components/App';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(thunk)),
