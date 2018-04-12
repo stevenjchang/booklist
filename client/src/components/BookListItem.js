76a130bc89d960c6a5012c4ab47baa652bc84846
Steve Chang <stevenjchang@gmail.com>
2018-04-11 20:40:55 -0700

    display img link

--- client/src/components/BookListItem.js
+++ client/src/components/BookListItem.js
@@ -10,12 +10,14 @@ class BookListItem extends React.Component {
     let title = book.volumeInfo ? book.volumeInfo.title : 'no-title';
     let price = book.saleInfo && book.saleInfo.retailPrice ? book.saleInfo.retailPrice.amount : 0.00;
     let publishedDate = book.volumeInfo ? book.volumeInfo.publishedDate : 'no-published-date';
+    let image = book.volumeInfo && book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : 'www.noimage.com';
 
     return (
       <div>BookListItem: {book.id} 
         <ul>
           <li>title: {title} ${price}</li>
           <li>publishedDate: {publishedDate}</li>
+          <img src={image} />
         </ul>
       
       </div>
